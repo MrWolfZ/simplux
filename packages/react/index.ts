08226e9544eb38cf0957e5d9af1daec50c8cc792
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-02 16:27:38 +0200

    fix(react): add missing exports to public API

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,4 +1,9 @@
 import { registerSimpluxModuleExtension } from '@simplux/core'
 import { reactModuleExtension } from './src/hooks'
 
+export {
+  SimpluxModuleReactExtensions,
+  SimpluxModuleSelectorHook,
+} from './src/hooks'
+
 registerSimpluxModuleExtension(reactModuleExtension)
