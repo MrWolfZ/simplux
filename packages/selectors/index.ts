d8c3513dd4c5bc7beb459ea1a03c52825216fb5b
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-02 16:24:09 +0200

    fix(selectors): add missing exports to public API

--- packages/selectors/index.ts
+++ packages/selectors/index.ts
@@ -1,4 +1,14 @@
 import { registerSimpluxModuleExtension } from '@simplux/core'
 import { selectorsModuleExtension } from './src/selectors'
 
+export {
+  ResolvedSelector,
+  ResolvedSelectorExtras,
+  ResolvedSelectors,
+  SelectorBase,
+  SelectorsBase,
+  SelectorsFactory,
+  SimpluxModuleSelectorExtensions,
+} from './src/selectors'
+
 registerSimpluxModuleExtension(selectorsModuleExtension)
