aed2ca45374fc972699e2b51ed429b868d7fcaad
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-05 20:45:14 +0200

    feat(recipes): simplify "getting started" recipe

--- recipes/basics/getting-started/src/index.ts
+++ recipes/basics/getting-started/src/index.ts
@@ -3,81 +3,58 @@ import {
   getSimpluxReducer,
   setReduxStoreForSimplux,
 } from '@simplux/core'
-import { combineReducers, createStore } from 'redux'
+import { createStore } from 'redux'
 
-// first, we create our root reducer and the redux store (in an
-// existing application you would simply use your existing reducer
-// and store)
-const rootReducer = combineReducers({
-  // this key can be whatever you desire
-  simplux: getSimpluxReducer(),
-  // plus any of your other reducers
-})
+// this code is part of the recipe for getting started with
+// simplux, which you can find here:
+// https://github.com/MrWolfZ/simplux/tree/master/recipes/basics/getting-started
 
-const store = createStore(rootReducer)
+// first, we create a redux store that only contains simplux state
+const store = createStore(getSimpluxReducer())
 
 // then, we tell simplux to use our store; the second argument
 // exists to tell simplux where it can find its state on the
-// root state
-setReduxStoreForSimplux(store, rootState => rootState.simplux)
+// root state, which in this case is the root state itself
+setReduxStoreForSimplux(store, s => s)
 
-// now we create our module and destructure it into all
-// the functions we are going to use
-const {
-  getState,
-  setState,
-  subscribeToStateChanges,
-  createMutations,
-} = createSimpluxModule({
+// state in simplux is organized into modules; here we create
+// our first simple counter module
+const counterModule = createSimpluxModule({
+  // this name uniquely identifies our module
   name: 'counter',
+
+  // this value determines the shape of our state
   initialState: {
     counter: 0,
   },
 })
 
-// you can get the current state via getState
-console.log('initial state:', getState())
+// you can get the current state of the module via getState
+console.log('initial state:', counterModule.getState())
 
-// you can update the whole state at once
-setState({ counter: 1 })
-console.log('updated state:', getState())
-
-// you can get notified whenever the state changes
-const unsubscribe = subscribeToStateChanges(state =>
-  console.log('state was updated:', state),
-)
-
-setState({ counter: 2 })
-
-// instead of directly setting the state, usually it is better
-// to define mutations
-const { increment, incrementBy } = createMutations({
+// to change the state, we can define mutations; a mutation
+// is a pure function that takes the current module state and
+// optionally some additional arguments and returns a new
+// updated state
+const { increment, incrementBy } = counterModule.createMutations({
+  // we can have mutations that only use the state
   increment: state => ({ ...state, counter: state.counter + 1 }),
+
+  // our mutations can also have arguments
   incrementBy: (state, amount: number) => ({
     ...state,
     counter: state.counter + amount,
   }),
 })
 
-// to update the module state, just call the mutation
+// to update the module state, simply call a mutation (this
+// dispatches a redux action internally)
 increment()
-console.log('incremented counter:', getState())
+console.log('incremented counter:', counterModule.getState())
 
-// we can also pass arguments to the mutation
+// we can also pass arguments to a mutation
 incrementBy(5)
-console.log('incremented counter by 5:', getState())
-
-// you can define new mutations at any time (but you cannot
-// overwrite an existing mutation)
-const { decrement } = createMutations({
-  decrement: s => ({ ...s, counter: s.counter - 1 }),
-})
-
-decrement()
-console.log('decremented counter:', getState())
-
-// if we want to stop getting notified, we just unsubscribe
-unsubscribe()
+console.log('incremented counter by 5:', counterModule.getState())
 
 // executing a mutation returns the updated state
-console.log('final state:', decrement())
+console.log('final state:', increment())
