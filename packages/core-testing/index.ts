af4bbd866ff5d4bfedafe03bb6b31643f7bbab86
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-10 11:33:43 +0200

    fix(core-testing): add missing API exports

--- packages/core-testing/index.ts
+++ packages/core-testing/index.ts
@@ -1,4 +1,6 @@
 import { registerSimpluxModuleExtension } from '@simplux/core'
 import { mutationsTestingModuleExtension } from './src/mutations'
 
+export { MutationsMocks, ResolvedMutationTestingExtras } from './src/mutations'
+
 registerSimpluxModuleExtension(mutationsTestingModuleExtension, 200)
