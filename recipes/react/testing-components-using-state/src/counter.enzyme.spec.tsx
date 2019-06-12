b9b789ad958256b561221b586d382859c94b191c
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-12 20:35:59 +0200

    docs(recipes): add inline comments to "testing components using state"

--- recipes/react/testing-components-using-state/src/counter.enzyme.spec.tsx
+++ recipes/react/testing-components-using-state/src/counter.enzyme.spec.tsx
@@ -18,7 +18,15 @@ import {
   useCounter,
 } from './counter-module'
 
+// this file shows you how to test your components that read
+// and change state; here we are using enzyme but any other
+// test renderer would also work
+
 describe(Counter.name, () => {
+  // one possible way to test your component that reads state
+  // is to test it with the real module by setting its state
+  // before rendering the component; this is more of an
+  // integration style of testing
   it('displays the value', () => {
     setCounterValue(10)
 
@@ -28,8 +36,17 @@ describe(Counter.name, () => {
     expect(wrapper.contains(expected)).toBe(true)
   })
 
+  // however, usually it is better to test your component in
+  // isolation without interacting with with the real module;
+  // this is where the react-testing extension comes into play;
+  // if you are using the module's selector hook it allows you
+  // to mock the state that the hook will use; instead of using
+  // the real module's state it will call the selector with the
+  // provided mock state
   it('displays the value times two (mocked)', () => {
-    mockSelectorHookState(useCounter, { value: 20 })
+    mockSelectorHookState(useCounter, {
+      value: 20,
+    })
 
     const wrapper = shallow(<Counter />)
     const expected = <span>value * 2: 40</span>
@@ -37,13 +54,24 @@ describe(Counter.name, () => {
     expect(wrapper.contains(expected)).toBe(true)
   })
 
+  // the mockSelectorHookState call above mocks the state
+  // indefinitely; therefore we need to make sure that we remove
+  // the mocked state after each test
   afterEach(() => {
+    // we can remove the mock state for a single selector hook
     removeSelectorHookMockState(useCounter)
 
+    // alternatively we can also just remove all selector hook mocks
     removeAllSelectorHookMockStates()
   })
 
+  // for your convenience it is also possible to mock the state
+  // just for the next render in which the selector hook is used
+  // so that you do not need to actively remove it
   it('displays the value times five (mocked during render)', () => {
+    // this will only mock the state for the useCounter hook during
+    // the next render; this also works for nested components that
+    // use the hook
     mockSelectorHookStateForNextRender(useCounter, { value: 30 })
 
     const wrapper = shallow(<Counter />)
@@ -52,6 +80,11 @@ describe(Counter.name, () => {
     expect(wrapper.contains(expected)).toBe(true)
   })
 
+  // testing your components that trigger mutations can also be
+  // done with the real module by setting the module's state before
+  // rendering, then triggering the mutation, and finally checking
+  // the module's state; this is once again more of an integration
+  // style of testing
   it('triggers an increment when the "Increment" button is clicked', () => {
     setCounterValue(10)
 
@@ -64,6 +97,10 @@ describe(Counter.name, () => {
     expect(getCounterValue()).toBe(11)
   })
 
+  // however, the recommended way to test these components is to mock
+  // the selector hooks state (as we saw above) and to also mock the
+  // mutation (see the recipe for "testing my code that uses mutations"
+  // for more details about this)
   it('triggers an increment by 5 when the "Increment by 5" button is clicked', () => {
     mockSelectorHookStateForNextRender(useCounter, { value: 10 })
     const incrementBySpy = mockMutationOnce(incrementBy, jest.fn())
--- recipes/react/testing-components-using-state/src/counter.testing-library.spec.tsx
+++ recipes/react/testing-components-using-state/src/counter.testing-library.spec.tsx
@@ -18,9 +18,17 @@ import {
   useCounter,
 } from './counter-module'
 
+// this file shows you how to test your components that read
+// and change state; here we are using the react testing library
+// but any other test renderer would also work
+
 describe(Counter.name, () => {
   afterEach(cleanup)
 
+  // one possible way to test your component that reads state
+  // is to test it with the real module by setting its state
+  // before rendering the component; this is more of an
+  // integration style of testing
   it('displays the value', () => {
     setCounterValue(10)
 
@@ -29,21 +37,41 @@ describe(Counter.name, () => {
     expect(getByText(/value:\s*10/g)).toBeDefined()
   })
 
+  // however, usually it is better to test your component in
+  // isolation without interacting with with the real module;
+  // this is where the react-testing extension comes into play;
+  // if you are using the module's selector hook it allows you
+  // to mock the state that the hook will use; instead of using
+  // the real module's state it will call the selector with the
+  // provided mock state
   it('displays the value times two (mocked)', () => {
-    mockSelectorHookState(useCounter, { value: 20 })
+    mockSelectorHookState(useCounter, {
+      value: 20,
+    })
 
     const { getByText } = render(<Counter />)
 
     expect(getByText(/value \* 2:\s*40/g)).toBeDefined()
   })
 
+  // the mockSelectorHookState call above mocks the state
+  // indefinitely; therefore we need to make sure that we remove
+  // the mocked state after each test
   afterEach(() => {
+    // we can remove the mock state for a single selector hook
     removeSelectorHookMockState(useCounter)
 
+    // alternatively we can also just remove all selector hook mocks
     removeAllSelectorHookMockStates()
   })
 
+  // for your convenience it is also possible to mock the state
+  // just for the next render in which the selector hook is used
+  // so that you do not need to actively remove it
   it('displays the value times five (mocked during render)', () => {
+    // this will only mock the state for the useCounter hook during
+    // the next render; this also works for nested components that
+    // use the hook
     mockSelectorHookStateForNextRender(useCounter, { value: 30 })
 
     const { getByText } = render(<Counter />)
@@ -51,6 +79,11 @@ describe(Counter.name, () => {
     expect(getByText(/value \* 5:\s*150/g)).toBeDefined()
   })
 
+  // testing your components that trigger mutations can also be
+  // done with the real module by setting the module's state before
+  // rendering, then triggering the mutation, and finally checking
+  // the module's state; this is once again more of an integration
+  // style of testing
   it('triggers an increment when the "Increment" button is clicked', () => {
     setCounterValue(10)
 
@@ -61,6 +94,10 @@ describe(Counter.name, () => {
     expect(getCounterValue()).toBe(11)
   })
 
+  // however, the recommended way to test these components is to mock
+  // the selector hooks state (as we saw above) and to also mock the
+  // mutation (see the recipe for "testing my code that uses mutations"
+  // for more details about this)
   it('triggers an increment by 5 when the "Increment by 5" button is clicked', () => {
     mockSelectorHookStateForNextRender(useCounter, { value: 10 })
     const incrementBySpy = mockMutationOnce(incrementBy, jest.fn())
