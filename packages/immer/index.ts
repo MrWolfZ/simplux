75ec5d5aba7be1e884f0738ae55fc4d9e33af4d0
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-22 12:38:10 +0200

    refactor(immer): remove obsolete extension package

--- packages/immer/index.ts
+++
@@ -1,6 +0,0 @@
-import { registerSimpluxModuleExtension } from '@simplux/core'
-import { immerModuleExtension } from './src/immer'
-
-export { ImmerMutationReturnTypeOverride } from './src/immer'
-
-registerSimpluxModuleExtension(immerModuleExtension, 110)
