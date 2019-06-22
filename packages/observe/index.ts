9821cc1c0728863377543db73ea8f8cc7cc9b0b9
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-22 12:20:10 +0200

    refactor(observe): remove obsolete extension package

--- packages/observe/index.ts
+++
@@ -1,6 +0,0 @@
-import { registerSimpluxModuleExtension } from '@simplux/core'
-import { observeModuleExtension } from './src/observe'
-
-export { SimpluxModuleObserveExtensions } from './src/observe'
-
-registerSimpluxModuleExtension(observeModuleExtension, 140)
