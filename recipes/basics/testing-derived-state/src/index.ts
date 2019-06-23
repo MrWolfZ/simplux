43b3c576ed6eda23baa3286cf803af83b8bca9ee
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-23 14:26:23 +0200

    refactor(recipes): fix errors in "testing derived state"

--- recipes/basics/testing-derived-state/src/index.ts
+++ recipes/basics/testing-derived-state/src/index.ts
@@ -4,6 +4,6 @@
 // this file only exists for code sandbox to execute correctly;
 // for the tests please see the counter.module.spec.ts file
 
-import { getCounterState } from './counter.module'
+import { counterModule } from './counter.module'
 
-console.log(`counter module state:`, getCounterState())
+console.log(`counter module state:`, counterModule.getState())
