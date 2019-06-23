75b0c9af2be7760e5c035280edfac6e8eba76f8a
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-06-23 09:14:47 +0200

    docs(recipes): minor tweak to "testing my state changes"

--- recipes/basics/testing-state-changes/src/index.ts
+++ recipes/basics/testing-state-changes/src/index.ts
@@ -1,6 +1,9 @@
 // this code is part of the simplux recipe "testing state changes":
 // https://github.com/MrWolfZ/simplux/tree/master/recipes/basics/testing-state-changes
 
+// this file only exists for code sandbox to execute correctly;
+// for the tests please see the counter.module.spec.ts file
+
 import { getCounterState } from './counter.module'
 
 console.log(`counter module state:`, getCounterState())
