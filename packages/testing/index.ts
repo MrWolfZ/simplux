0f44b67cc2424cfc7bc0364de69654318a30f618
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-09-15 16:29:38 +0200

    fix(testing): add missing export

--- packages/testing/index.ts
+++ packages/testing/index.ts
@@ -2,5 +2,6 @@ export {
   clearAllSimpluxMocks,
   registerMockCleanupFunction,
 } from './src/cleanup'
+export { mockEffect } from './src/effects'
 export { mockModuleState } from './src/module'
 export { mockMutation } from './src/mutations'
