ad26a853c7cb96cba03eb5901f9eeefc18f16e8a
Jonathan Ziller <jonathan.ziller@gmail.com>
2020-04-12 16:48:37 +0200

    fix(core): properly infer mutation types for immutable state type

--- packages/core/src/mutations.ts
+++ packages/core/src/mutations.ts
@@ -40,7 +40,10 @@ export interface SimpluxMutation<TState, TArgs extends any[]> {
    *
    * @returns the updated state
    */
-  readonly withState: (state: TState, ...args: TArgs) => TState
+  readonly withState: (
+    state: TState | Mutable<TState>,
+    ...args: TArgs
+  ) => TState
 
   /**
    * When a mutation is called directly it updates the module's state by
@@ -62,7 +65,10 @@ type ShallowMutable<T> = { -readonly [prop in keyof T]: T[prop] }
 export type ResolvedMutation<
   TState,
   TMutation extends MutationDefinition<TState>
-> = TMutation extends (state: TState, ...args: infer TArgs) => TState | void
+> = TMutation extends (
+  state: Mutable<TState>,
+  ...args: infer TArgs
+) => TState | void
   ? SimpluxMutation<TState, TArgs>
   : never
 
@@ -180,7 +186,7 @@ export function createMutations<
 
       const extras = mutation as ShallowMutable<typeof mutation>
 
-      extras.withState = (state: TState, ...args: any[]) => {
+      extras.withState = (state: TState | Mutable<TState>, ...args: any[]) => {
         return getReducer()(state, createAction(...args))
       }
 
