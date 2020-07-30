43c266e1898fdfba5de3923a14531543698e7fae
Jonathan Ziller <jonathan.ziller@gmail.com>
2020-07-30 12:18:57 +0200

    fix(core): handle arrays, sets, and maps properly in utility types "Mutable" and "Immutable"

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -8,7 +8,7 @@
 // immutable; obviously this adds a cost to compilation time, but so
 // far, I have not experienced any too adverse effects; should performance
 // become an issue in the future, simply drop the IfMutable/IfImmutable
-// branches
+// branches; in addition, we exclude some tricky cases like maps and sets
 
 type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X
   ? 1
@@ -35,8 +35,6 @@ type ReadonlyKeys<T> = {
 
 type AtomicObject =
   | Function
-  | WeakMap<any, any>
-  | WeakSet<any>
   | Promise<any>
   | Date
   | RegExp
@@ -44,13 +42,11 @@ type AtomicObject =
   | Number
   | String
 
+type MutableArray<T> = T extends (infer U)[] ? Mutable<U>[] : never
+
 type IsMutable<T> = T extends AtomicObject
   ? true
-  : T extends Map<any, any>
-  ? true
-  : T extends Set<any>
-  ? true
-  : T extends any[]
+  : T extends MutableArray<T>
   ? true
   : T extends object
   ? Exclude<keyof T, WritableKeys<T>> extends never
@@ -62,17 +58,13 @@ type IsMutable<T> = T extends AtomicObject
     : false
   : true
 
+type ImmutableArray<T> = T extends readonly (infer U)[]
+  ? readonly Immutable<U>[]
+  : any
+
 type IsImmutable<T> = T extends AtomicObject
   ? true
-  : T extends Map<any, any>
-  ? Omit<Map<any, any>, 'set' | 'delete' | 'clear'> extends T
-    ? true
-    : false
-  : T extends Set<any>
-  ? Omit<Set<any>, 'add' | 'delete' | 'clear'> extends T
-    ? true
-    : false
-  : readonly any[] extends T
+  : ImmutableArray<T> extends T
   ? true
   : T extends object
   ? Exclude<keyof T, ReadonlyKeys<T>> extends never
@@ -88,9 +80,11 @@ export type Mutable<T> = T extends AtomicObject
   ? T
   : IsMutable<T> extends true
   ? T
-  : T extends Map<infer K, infer V>
+  : T extends readonly (infer U)[]
+  ? Mutable<U>[]
+  : T extends ReadonlyMap<infer K, infer V>
   ? Map<Mutable<K>, Mutable<V>>
-  : T extends Set<infer V>
+  : T extends ReadonlySet<infer V>
   ? Set<Mutable<V>>
   : T extends object
   ? { -readonly [K in keyof T]: Mutable<T[K]> }
@@ -101,9 +95,9 @@ export type Immutable<T> = T extends AtomicObject
   : IsImmutable<T> extends true
   ? T
   : T extends Map<infer K, infer V>
-  ? Omit<Map<K, V>, 'set' | 'delete' | 'clear'>
+  ? ReadonlyMap<Immutable<K>, Immutable<V>>
   : T extends Set<infer V>
-  ? Omit<Set<V>, 'add' | 'delete' | 'clear'>
+  ? ReadonlySet<Immutable<V>>
   : T extends object
   ? { readonly [K in keyof T]: Immutable<T[K]> }
   : T
--- packages/core/test-d/types.test-d.ts
+++ packages/core/test-d/types.test-d.ts
@@ -9,6 +9,9 @@ interface S {
     foo: number
     bar: string[]
   }
+  baz: {
+    bar: string[]
+  }[]
 }
 
 interface S2 {
@@ -17,6 +20,9 @@ interface S2 {
     readonly foo: number
     bar: string[]
   }
+  baz: readonly {
+    bar: string[]
+  }[]
 }
 
 interface S3 {
@@ -25,6 +31,9 @@ interface S3 {
     foo: number
     bar: readonly string[]
   }
+  baz: {
+    bar: readonly string[]
+  }[]
 }
 
 interface S4 {
@@ -33,24 +42,123 @@ interface S4 {
     readonly foo: number
     readonly bar: readonly string[]
   }
+  readonly baz: readonly {
+    readonly bar: readonly string[]
+  }[]
 }
+
+interface S5 {
+  bar: {
+    set: Set<string[]>
+    weakSet: WeakSet<string[]>
+    map: Map<string, string[]>
+  }
+}
+
+interface S6 {
+  readonly bar: {
+    readonly set: ReadonlySet<readonly string[]>
+    readonly weakSet: WeakSet<readonly string[]>
+    readonly map: ReadonlyMap<string, readonly string[]>
+  }
+}
+
+expectType<string>(undefined! as Mutable<string>)
+expectType<number>(undefined! as Mutable<number>)
+expectType<boolean>(undefined! as Mutable<boolean>)
+expectType<undefined>(undefined! as Mutable<undefined>)
+expectType<null>(undefined! as Mutable<null>)
+expectType<string | undefined>(undefined! as Mutable<string | undefined>)
+expectType<Date>(undefined! as Mutable<Date>)
+expectType<Function>(undefined! as Mutable<Function>)
+expectType<RegExp>(undefined! as Mutable<RegExp>)
+expectType<Promise<string>>(undefined! as Mutable<Promise<string>>)
+
+expectType<string[]>(undefined! as Mutable<readonly string[]>)
+expectType<{ foo: string }[]>(undefined! as Mutable<readonly { foo: string }[]>)
+expectType<{ foo: string }[]>(undefined! as Mutable<
+  readonly { readonly foo: string }[]
+>)
 
 expectType<{ foo: string }>(undefined! as Mutable<{ readonly foo: string }>)
 expectType<{ foo: string[] }>(undefined! as Mutable<{
   readonly foo: readonly string[]
 }>)
 
+expectType<Set<string>>(undefined! as Mutable<ReadonlySet<string>>)
+expectType<Set<string[]>>(undefined! as Mutable<ReadonlySet<string[]>>)
+expectType<Set<string[]>>(undefined! as Mutable<ReadonlySet<readonly string[]>>)
+
+expectType<WeakSet<string[]>>(undefined! as Mutable<WeakSet<string[]>>)
+expectType<WeakSet<string[]>>(undefined! as Mutable<WeakSet<readonly string[]>>)
+
+expectType<Map<string, string>>(undefined! as Mutable<
+  ReadonlyMap<string, string>
+>)
+expectType<Map<string, string[]>>(undefined! as Mutable<
+  ReadonlyMap<string, string[]>
+>)
+expectType<Map<string, string[]>>(undefined! as Mutable<
+  ReadonlyMap<string, readonly string[]>
+>)
+
 expectType<S>(undefined! as Mutable<S>)
 expectType<S>(undefined! as Mutable<S2>)
 expectType<S>(undefined! as Mutable<S3>)
 expectType<S>(undefined! as Mutable<S4>)
+expectType<S5>(undefined! as Mutable<S5>)
+expectType<S5>(undefined! as Mutable<S6>)
 
-expectType<{ readonly foo: string }>(undefined! as Immutable<{ foo: string }>)
-expectType<{ readonly foo: readonly string[] }>(undefined! as Immutable<{
-  foo: string[]
-}>)
+expectType<string>(undefined! as Immutable<string>)
+expectType<number>(undefined! as Immutable<number>)
+expectType<boolean>(undefined! as Immutable<boolean>)
+expectType<undefined>(undefined! as Immutable<undefined>)
+expectType<null>(undefined! as Immutable<null>)
+expectType<string | undefined>(undefined! as Immutable<string | undefined>)
+expectType<Date>(undefined! as Immutable<Date>)
+expectType<Function>(undefined! as Immutable<Function>)
+expectType<RegExp>(undefined! as Immutable<RegExp>)
+expectType<Promise<string>>(undefined! as Immutable<Promise<string>>)
 
-expectType<S4>(undefined! as Immutable<S>)
-expectType<S4>(undefined! as Immutable<S2>)
-expectType<S4>(undefined! as Immutable<S3>)
-expectType<S4>(undefined! as Immutable<S4>)
+expectType<Immutable<string[]>>(undefined! as readonly string[])
+expectType<Immutable<{ foo: string }[]>>(undefined! as readonly {
+  foo: string
+}[])
+expectType<Immutable<{ foo: string }[]>>(undefined! as readonly {
+  readonly foo: string
+}[])
+
+expectType<Immutable<{ foo: string }>>(undefined! as { readonly foo: string })
+expectType<
+  Immutable<{
+    foo: string[]
+  }>
+>(undefined! as { readonly foo: readonly string[] })
+
+expectType<Immutable<Set<string>>>(undefined! as ReadonlySet<string>)
+expectType<Immutable<Set<string[]>>>(undefined! as ReadonlySet<
+  readonly string[]
+>)
+expectType<Immutable<Set<readonly string[]>>>(undefined! as ReadonlySet<
+  readonly string[]
+>)
+
+expectType<Immutable<Map<string, string>>>(undefined! as ReadonlyMap<
+  string,
+  string
+>)
+expectType<Immutable<Map<string, string[]>>>(undefined! as ReadonlyMap<
+  string,
+  readonly string[]
+>)
+expectType<Immutable<Map<string, readonly string[]>>>(undefined! as ReadonlyMap<
+  string,
+  readonly string[]
+>)
+
+expectType<Immutable<S>>(undefined! as S4)
+expectType<Immutable<S2>>(undefined! as S4)
+expectType<Immutable<S3>>(undefined! as S4)
+expectType<Immutable<S4>>(undefined! as S4)
+expectType<Immutable<S5>>(undefined! as S6)
+expectType<Immutable<S6>>(undefined! as S6)
