7e9917e3f44e9f9c189debf794d23e7e9a8e4dc7
Jonathan Ziller <jonathan.ziller@gmail.com>
2020-12-18 19:28:08 +0100

    test(core): fix type tests

--- packages/core/test-d/effects.test-d.ts
+++ packages/core/test-d/effects.test-d.ts
@@ -1,8 +1,8 @@
 import { expectError, expectType } from 'tsd'
-import { createEffect, createEffects, Effect } from '../src/effects'
+import { createEffect, createEffects, SimpluxEffect } from '../src/effects'
 
-expectType<Effect<(s: string) => string>>(createEffect((s: string) => s))
-expectType<Effect<(s: string) => Promise<string>>>(
+expectType<SimpluxEffect<(s: string) => string>>(createEffect((s: string) => s))
+expectType<SimpluxEffect<(s: string) => Promise<string>>>(
   createEffect(async (s: string) => s),
 )
 
@@ -14,9 +14,9 @@ expectError(createEffect(''))
 // tslint:disable: type-literal-delimiter
 // tslint:disable: trailing-comma
 expectType<{
-  one: Effect<(s: string) => string>
-  two: Effect<(n: number) => number>
-  three: Effect<(s: string) => Promise<string>>
+  one: SimpluxEffect<(s: string) => string>
+  two: SimpluxEffect<(n: number) => number>
+  three: SimpluxEffect<(s: string) => Promise<string>>
 }>(
   createEffects({
     one: (s: string) => s,
