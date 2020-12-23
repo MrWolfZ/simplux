f2252863a0b9b601c47cfff067124433d3b0a0ad
Jonathan Ziller <jonathan.ziller@gmail.com>
2020-12-23 11:15:44 +0100

    test(core): improve type tests for effects

--- packages/core/test-d/effects.test-d.ts
+++ packages/core/test-d/effects.test-d.ts
@@ -6,8 +6,12 @@ expectType<SimpluxEffect<(s: string) => Promise<string>>>(
   createEffect(async (s: string) => s),
 )
 
-const effect = createEffect(<T>(s: T) => s)
-expectType<string>(effect<string>(''))
+const genericEffect = createEffect(<T>(s: T) => s)
+expectType<SimpluxEffect<<T>(s: T) => T>>(genericEffect)
+expectType<SimpluxEffect<(s: string) => string>>(genericEffect)
+expectType<<T>(s: T) => T>(genericEffect)
+expectType<(s: string) => string>(genericEffect)
+expectType<string>(genericEffect<string>(''))
 
 // @ts-expect-error
 expectError(createEffect(''))
@@ -20,28 +24,41 @@ interface FunctionWithProps {
 
 const functionWithProps: FunctionWithProps = undefined!
 
-// @ts-expect-error
-expectError(createEffect(functionWithProps).someProp)
-
-// tslint:disable: type-literal-delimiter
-// tslint:disable: trailing-comma
-expectType<{
-  one: SimpluxEffect<(s: string) => string>
-  two: SimpluxEffect<(n: number) => number>
-  three: SimpluxEffect<(s: string) => Promise<string>>
-}>(
-  createEffects({
-    one: (s: string) => s,
-    two: (n: number) => n,
-    three: async (s: string) => s,
-  }),
-)
-
-const effects = createEffects({ genericEffect: <T>(s: T) => s })
-expectType<string>(effects.genericEffect<string>(''))
+const effectWithProps = createEffect(functionWithProps)
+expectType<SimpluxEffect<(s: string) => string>>(effectWithProps)
+expectType<(s: string) => string>(effectWithProps)
 
 // @ts-expect-error
-expectError(createEffects(''))
+expectError(effectWithProps.someProp)
+
+const effects = createEffects({
+  one: (s: string) => s,
+  two: (n: number) => n,
+  three: async (s: string) => s,
+  genericEffect: <T>(s: T) => s,
+  functionWithProps,
+})
+
+expectType<SimpluxEffect<(s: string) => string>>(effects.one)
+expectType<(s: string) => string>(effects.one)
+
+expectType<SimpluxEffect<(n: number) => number>>(effects.two)
+expectType<(n: number) => number>(effects.two)
+
+expectType<SimpluxEffect<(s: string) => Promise<string>>>(effects.three)
+expectType<(s: string) => Promise<string>>(effects.three)
+
+expectType<SimpluxEffect<<T>(s: T) => T>>(effects.genericEffect)
+expectType<SimpluxEffect<(s: string) => string>>(effects.genericEffect)
+expectType<<T>(s: T) => T>(effects.genericEffect)
+expectType<(s: string) => string>(effects.genericEffect)
+expectType<string>(effects.genericEffect('' as string))
+
+expectType<SimpluxEffect<(s: string) => string>>(effects.functionWithProps)
+expectType<(s: string) => string>(effects.functionWithProps)
 
 // @ts-expect-error
 expectError(createEffects({ functionWithProps }).functionWithProps.someProp)
+
+// @ts-expect-error
+expectError(createEffects(''))
